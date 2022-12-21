3ac36ecab7f88fb3d36a364b5d778b8e3ab4e3f1
Calegria27 <carlos.alegria@cindependencia.cl>
2022-12-21 11:43:41 -0300

    Uf a drive

--- UF.py
+++ UF.py
@@ -5,6 +5,9 @@ from datetime import datetime, timezone, timedelta
 import pytz
 from pytz import timezone
 import pypyodbc as odbc 
+from Google import create_service
+import numpy as np
+
 
 year_today = datetime.now(timezone('UTC')).strftime("%Y")
 url = "https://mindicador.cl/api/uf/"+year_today
@@ -75,3 +78,68 @@ finally:
     print('Task is complete.')
     cursor.close()
     conn.close()
+
+
+data_uf2=data_uf
+data_uf2['fecha'] = pd.to_datetime(data_uf2.fecha)
+data_uf2=data_uf2.sort_values(by='fecha',ascending=True)
+data_uf2['fecha']=data_uf2['fecha'].dt.strftime('%d/%m/%Y')
+data_uf2['valor']=data_uf2['valor'].map('{:,.2f}'.format)
+data_uf2 = data_uf2.replace(',', 'x', regex=True)
+data_uf2 = data_uf2.replace('\.', ',', regex= True)
+data_uf2 = data_uf2.replace('x', '.', regex= True)
+data_uf2=data_uf2.drop(columns=['Tipo divisa'])
+
+
+"""
+Getting  Google Sheets
+"""
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+services = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION,SCOPES )
+
+google_sheets_id = '1iZPHRQNhMk8o7LQzVxkLc7hUnG0NuMFy9Zw1UlVGuS0'
+
+"""
+Insert dataset
+"""
+def construct_request_body(value_array, dimension: str='ROWS') -> dict:
+    try:
+        request_body = {
+            'majorDimension': dimension,
+            'values': value_array
+        }
+        return request_body
+    except Exception as e:
+        print(e)
+        return {}
+
+response = services.spreadsheets().values().get(
+    spreadsheetId=google_sheets_id,
+    majorDimension='ROWS',
+    range='UF'
+    ).execute()
+
+def in_list(list1,list2):
+    df=pd.DataFrame(columns=['fecha','valor'])
+    for i in list1:
+        if i not in list2:
+            df.loc[len(df)] = i
+    return df
+
+df_aux=in_list(data_uf2.values.tolist(),response["values"])
+
+if(len(df_aux)>0):
+    request_body_values = construct_request_body(df_aux.values.tolist())
+    services.spreadsheets().values().append(
+            spreadsheetId=google_sheets_id,
+            valueInputOption='USER_ENTERED',
+            range="UF",
+            body=request_body_values
+        ).execute()
+    print("UF ingresada en Drive")
+else:
+    print("Valores ya estan ingresados en Drive")
